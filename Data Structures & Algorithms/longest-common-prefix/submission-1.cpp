d1154a399322fdd18104db02db707379fe43ef4c
Suryansh Shukla <suryanshshukla23@lpu.in>
2026-08-18 00:27:36 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(), strs.end());
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
+        string ans = "";
+        for (int i = 0; i < min(first.size(),last.size()); i++) {
+            if (first[i] == last[i]) ans.push_back(first[i]);
+            else return ans;
+        }
+        return ans;
+    }
+};
